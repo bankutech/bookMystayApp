c357040d44dc36904a6af2ad3194268403c41138
Sagnik Mitra <sagnikmitra1302@gmail.com>
2026-03-18 11:05:16 +0530

    Use Case 5: Booking Request (First-Come-First-Served)

--- UseCase5BookingRequestQueue.java
+++ UseCase5BookingRequestQueue.java
@@ -51,7 +51,6 @@ class BookingQueue {
     }
 }
 
-
 public class UseCase5BookingRequestQueue {
     public static void main(String[] args) {
         System.out.println("******************************************");
